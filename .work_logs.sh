4cb3d3e8923fb76ba44f88238d0ff9c0021b203e
lanshi <yhvguk@foxmail.com>
2025-11-09 22:30:27 +0800

    update fix bug

---
+++ .work_logs.sh
@@ -0,0 +1,11 @@
+: 1762692624:0;cd ~/Downloads
+: 1762692642:0;wine uninstall
+: 1762692644:0;wine uninstaller
+: 1762692761:0;python3 install.py
+: 1762693373:0;cd ~/LanguageTool
+: 1762693385:0;uv run gui.py
+: 1762693430:0;nolookup uv run gui.py
+: 1762693535:0;nohup uv run gui.py >output.log 2>&1 &
+: 1762694450:0;python3 install.py --build-dir /tmp/ycm_build_debug
+: 1762694584:0;python3 install.py 
+: 1762694628:0;vim
